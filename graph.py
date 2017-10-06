56bb487099c844d5d11fedf8417f1422597a9ebd
Adil <asadik@hbk.com>
2017-10-06 19:17:01 -0400

    Add link roles function

--- graph.py
+++ graph.py
@@ -25,12 +25,30 @@ def parse_single_file(file):
     # these are in the task key
     for task in y['tasks']:
         for key,value in task.items():
-            if key == 'include_role':
+            if qqqqqkey == 'include_role':
                 roles.append(value['name'])
 
     return(roles)
-    
 
-roles = parse_single_file('qs_desktop.yml')
-for role in roles:
-    print(role)
+def link_roles(dependent, depended):
+    """
+    Links the roles we've found
+    Dependent should be a role path
+    depended should be a list of roles on which
+    depended depends
+    """
+    dot = Digraph(comment='The Round Table')
+    dot.attr(size='18,50',layout='sfdp')
+    # create main node
+    dot.node('head', dependent)
+    for i, role in enumerate(depended):
+        dot.node(role)
+        dot.edge('head', role)
+    dot.render('test-output/round-table.gv', view=True)
+    print(dot.source)
+
+
+if __name__ == '__main__':
+    role_path = '/home/asadik/repos/playbooks/windows/qs_desktop.yml'
+    roles = parse_single_file(role_path)
+    link_roles(role_path, roles)
