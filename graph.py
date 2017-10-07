f549a8886402b0d65b0261fe29bf920d1deb85f9
Adil Sadik <sparky.005@gmail.com>
2017-10-06 20:58:44 -0400

    Fixed pylint issues

--- graph.py
+++ graph.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
 
 import yaml
-from graphviz import Graph, Digraph
+from graphviz import Digraph
 
 roles_base = './roles'
 
 def parse_single_file(file):
+    """Parses a single playbook"""
     roles = []
     with open(file, 'r') as f:
-        y = yaml.load(f)
+        playbook = yaml.load(f)
     # yaml gets loaded as a len 1 list of dicts
     # set y = 1 element, to make life easier
-    y = y[0]
+    playbook = playbook[0]
 
     # yaml file is broken down by header
     # first, we just get the listed roles in roles:
-    for role in y['roles']:
-        if type(role) is dict:
+    for role in playbook['roles']:
+        if isinstance(role, dict):
             roles.append(role['role'])
         else:
             roles.append(role)
 
     # next, we get the roles that are called by include_role
     # these are in the task key
-    for task in y['tasks']:
-        for key,value in task.items():
-            if qqqqqkey == 'include_role':
+    for task in playbook['tasks']:
+        for key, value in task.items():
+            if key == 'include_role':
                 roles.append(value['name'])
 
-    return(roles)
+    return roles
 
 def link_roles(dependent, depended):
     """
@@ -38,10 +39,10 @@ def link_roles(dependent, depended):
     depended depends
     """
     dot = Digraph(comment='The Round Table')
-    dot.attr(size='18,50',layout='sfdp')
+    dot.attr(size='18,50', layout='dot')
     # create main node
     dot.node('head', dependent)
-    for i, role in enumerate(depended):
+    for role in depended:
         dot.node(role)
         dot.edge('head', role)
     dot.render('test-output/round-table.gv', view=True)
