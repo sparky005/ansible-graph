b8a53a06bafe7a56b8c2559359ec1b2537702740
Adil Sadik <sparky.005@gmail.com>
2017-10-13 18:59:40 -0400

    Get rid of legacy single file parsing

--- graph.py
+++ graph.py
@@ -18,52 +18,6 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# i think here we should just return src, dest tuples
-# instead of a list of roles
-def parse_single_file(file):
-    """Parses a single playbook"""
-    roles = []
-    with open(file, 'r') as f:
-        playbook = yaml.load(f)
-    # yaml gets loaded as a len 1 list of dicts
-    # set y = 1 element, to make life easier
-    playbook = playbook[0]
-
-    # yaml file is broken down by header
-    # first, we just get the listed roles in roles:
-    for role in playbook['roles']:
-        # roles that have variables are dicts
-        if isinstance(role, dict):
-            roles.append(role['role'])
-        else:
-            roles.append(role)
-
-    # next, we get the roles that are called by include_role
-    # these are in the task key
-    for task in playbook['tasks']:
-        for key, value in task.items():
-            if key == 'include_role':
-                roles.append(value['name'])
-
-    return roles
-
-
-def link_roles(dependent, depended):
-    """
-    Links the roles we've found
-    Dependent should be a role path
-    depended should be a list of roles on which
-    depended depends
-    """
-    dot = Digraph(comment='The Round Table')
-    dot.attr(size='18,50', layout='dot')
-    dot.graph_attr['rankdir'] = 'LR'
-    # create main node
-    dot.node('head', dependent)
-    for role in depended:
-        dot.edge('head', role)
-    dot.render('test-output/round-table.gv', view=True)
-    print(dot.source)
 
 def find_nodes(roles_path):
     # we don't care about the following types of files
@@ -142,62 +96,52 @@ def parse_playbooks(nodes):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--file',
-        '-f',
-        help='File to generate graph',
-    )
-    parser.add_argument(
         '--roles-path',
         '-r',
         help='Path to roles directory'
     )
     args = parser.parse_args()
 
-    if args.file:
-        role_path = args.file
-        roles = parse_single_file(role_path)
-        link_roles(role_path, roles)
-    else:
-        roles_path = args.roles_path
-        nodes = find_nodes(roles_path)
-        # separate roles and playbooks
-        # TODO: refactor so that instead of splitting into two lists
-        # move the separation logic into a parse_all function
-        # and then parse each thing individually based on that
-        playbooks = [node for node in nodes if 'roles' not in node]
-        roles = [node for node in nodes if 'roles' in node]
+    roles_path = args.roles_path
+    nodes = find_nodes(roles_path)
+    # separate roles and playbooks
+    # TODO: refactor so that instead of splitting into two lists
+    # move the separation logic into a parse_all function
+    # and then parse each thing individually based on that
+    playbooks = [node for node in nodes if 'roles' not in node]
+    roles = [node for node in nodes if 'roles' in node]
 
-        logger.info("BEGIN PROCESSING PLAYBOOKS")
-        edges = parse_playbooks(playbooks)
-        logger.info("END PROCESSING PLAYBOOKS")
-        logger.info("BEGIN PROCESSING ROLES")
-        edges += parse_roles(roles)
-        logger.info("END PROCESSING ROLES")
+    logger.info("BEGIN PROCESSING PLAYBOOKS")
+    edges = parse_playbooks(playbooks)
+    logger.info("END PROCESSING PLAYBOOKS")
+    logger.info("BEGIN PROCESSING ROLES")
+    edges += parse_roles(roles)
+    logger.info("END PROCESSING ROLES")
 
-        # fix edge destinations to full paths
-        # that match the paths we have in roles
-        for i, edge in enumerate(edges):
-            for role in roles:
-                # rename role inclusions to point to the main.yml
-                if edge[1] in role and 'main.yml' in role:
-                    logger.info("Main role file found for %s", edge[1])
-                    logger.warning("Renaming %s to %s", edge[1], role)
-                    t = (edges[i][0], role)
-                    edges[i] = t
-                    break
-                # rename other inclusions to point to
-                # corresponding file
-                elif edge[1] in role:
-                    logger.info("Task file found for %s", edge[1])
-                    logger.warning("Renaming %s to %s", edge[1], role)
-                    new_edge = (edges[i][0], role)
-                    edges[i] = new_edge
+    # fix edge destinations to full paths
+    # that match the paths we have in roles
+    for i, edge in enumerate(edges):
+        for role in roles:
+            # rename role inclusions to point to the main.yml
+            if edge[1] in role and 'main.yml' in role:
+                logger.info("Main role file found for %s", edge[1])
+                logger.warning("Renaming %s to %s", edge[1], role)
+                t = (edges[i][0], role)
+                edges[i] = t
+                break
+            # rename other inclusions to point to
+            # corresponding file
+            elif edge[1] in role:
+                logger.info("Task file found for %s", edge[1])
+                logger.warning("Renaming %s to %s", edge[1], role)
+                new_edge = (edges[i][0], role)
+                edges[i] = new_edge
 
 
-        dot = Digraph(comment='Ansible Dependency Tree')
-        dot.attr(ranksep='10.2', nodesep='1.2', layout='dot')
-        dot.graph_attr['rankdir'] = 'LR'
-        for edge in edges:
-            dot.edge(edge[0], edge[1])
-        dot.render('test-output/round-table.gv', view=True)
-        print(dot.source)
+    dot = Digraph(comment='Ansible Dependency Tree', node_attr={'fontsize': '48'})
+    dot.attr(ranksep='10.2', nodesep='1.2', layout='dot')
+    dot.graph_attr['rankdir'] = 'LR'
+    for edge in edges:
+        dot.edge(edge[0], edge[1])
+    dot.render('test-output/round-table.gv', view=True)
+    print(dot.source)
