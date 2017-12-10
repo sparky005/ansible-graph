a2e6fd6cbd0891e2fc4daa4f95ee202f4979dbab
Adil Sadik <sparky.005@gmail.com>
2017-12-09 23:14:19 -0500

    fake filesystem, test for parse_playbooks

--- conftest.py
+++ conftest.py
@@ -1,5 +1,43 @@
 import pytest
+from pyfakefs.fake_filesystem_unittest import Patcher
 
 @pytest.fixture
 def roles_path():
-    return '../playbooks'
+    return '/tmp/playbooks'
+
+@pytest.fixture
+def playbooks():
+    return ['/tmp/playbooks/test_playbook.yml', '/tmp/playbooks/test2.yml']
+
+@pytest.fixture
+def roles():
+    return []
+
+@pytest.yield_fixture(scope='session')
+def fake_nodes():
+    """ Fake filesystem. """
+    patcher = Patcher()
+    patcher.setUp()
+    # TODO: create some linux playbooks in subdir
+    # for more realistic test
+    patcher.fs.CreateFile('/tmp/playbooks/test_playbook.yml',
+                        contents="""---
+                        - hosts: all
+                          gather_facts: true
+                          ignore_errors: true
+                        
+                          roles:
+                            - windows_common
+                            - { role: dept_common, app_version: 2 }
+                          tasks:
+                            - include_role:
+                                name: application/application2
+                        """)
+    patcher.fs.CreateFile('/tmp/playbooks/test2.yml',
+                        contents="""---
+                        - hosts: all
+                          tasks:
+                          """)
+    yield
+    patcher.tearDown()
+
--- test_graph.py
+++ test_graph.py
@@ -1,9 +1,16 @@
 import pytest
 from graph import *
+from pyfakefs.fake_filesystem_unittest import Patcher
 
-def test_find_nodes(roles_path):
+def test_find_nodes(roles_path, fake_nodes):
     nodes = find_nodes(roles_path)
-    assert roles_path == '../playbooks'
+    assert roles_path == '/tmp/playbooks'
     assert isinstance(nodes, list)
+    assert len(nodes) == 2
     assert isinstance(nodes[0], str)
     assert all('yml' in node or 'yaml' in node for node in nodes)
+
+def test_parse_playbooks(playbooks, fake_nodes):
+    assert isinstance(edges, list)
+    assert len(edges) == 3
+    assert isinstance(edges[0], tuple)
